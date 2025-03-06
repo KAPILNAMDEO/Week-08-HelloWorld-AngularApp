a79d6af47f19b63a650ecd00ccd2fcdd4575b9c3
kapil namdeo <kapilnamdeo2003@gmail.com>
2025-03-06 11:56:59 +0530

    [Kapil_Namdeo]Add.display error message in wrong input

--- HelloWorldApp/src/app/app.component.ts
+++ HelloWorldApp/src/app/app.component.ts
@@ -1,28 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FormsModule,RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  title = 'HelloWorldApp'; 
-  imgUrl = "assets/BridgeLabzLogo.jpg";
-  Url = "https://www.bridgelabz.com/";
+export class AppComponent {
+  title = 'HelloWorldApp';
+  imgUrl: string ='assets/BridgeLabzLogo.jpg';
+  url = "https://www.bridgelabz.com/";
   userName: string = "";
- 
+  nameError: string = "";
 
-  ngOnInit(): void {  
-    this.title = "Hello from BridgeLabz."; 
+  ngOnInit(): void{
+    this.title = "Hello from BridgeLabz."
   }
 
-  onClick($event:MouseEvent){
-    console.log("Save buttrameter '$event' implicitly has an 'any' type.ts(7006)on is clicked!",$event)
-    window.open(this.Url,"_blank")
+  onClick($event : MouseEvent){
+    console.log("Save Button is clicked!", $event);
+    window.open(this.url, "_blank");
+  }
+
+  onInput($event: InputEvent) {
+    console.log("change Event Occurred!", ($event.target as HTMLInputElement).value);
+    const nameRegex =RegExp('^[A-Z][a-zA-Z\s]{2,}$'); // Starts with uppercase, min 3 chars
+  
+    if (nameRegex.test(this.userName)) {
+      this.nameError = "";
+      return;
+    }
+      this.nameError = "Name is Incorrect!";
+    
   }
 }
-
